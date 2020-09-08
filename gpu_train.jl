8d9f13d9522f388f775cbc99c4ce290d35265030
Chris <chris.blake@emiratesteamnz.com>
2020-09-09 09:56:37 +1200

    delete mem on gpu

--- gpu_train.jl
+++ gpu_train.jl
@@ -196,7 +196,7 @@ function train()
         σ = std(adv_buf)
         adv_buf = (adv_buf .- μ) ./ σ
 
-        #shift to the gpu
+        # shift to the gpu
         actor = actor |> gpu
         act_log_std = act_log_std |> gpu
         act_params = params(actor, act_log_std)
@@ -222,9 +222,16 @@ function train()
         println("time to update value function: ", now() - st)
         println()
 
-        #shift to the cpu
+        # shift to the cpu
         actor = actor |> cpu
         act_log_std = act_log_std |> cpu
+
+        # free up memory on the gpu
+        states_buf = nothing
+        actions_buf = nothing
+        adv_buf = nothing
+        log_probs_buf = nothing
+        r2g_buf = nothing
         
     end
 end
